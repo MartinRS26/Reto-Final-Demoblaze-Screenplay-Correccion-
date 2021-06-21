d490f50cc9534e230224682793640340ec11a6f5
Martin Rodriguez <martinrodriii.2604@gmail.com>
2021-06-21 18:09:51 -0500

    Subiendo correcciones

--- src/main/java/com/demoblaze/automation/userinterfaces/CarritoPage.java
+++ src/main/java/com/demoblaze/automation/userinterfaces/CarritoPage.java
@@ -5,6 +5,6 @@ import net.serenitybdd.core.annotations.findby.By;
 
 
 public class CarritoPage {
-    public static final Target NOMBRE_PRODUCTO = Target.the("Nombre Producto del carrito").located(By.xpath("//*/tbody/tr[1]/td[2]"));
+    public static final Target NOMBRE_PRODUCTO = Target.the("Nombre Producto del carrito").located(By.xpath("//*/td[contains(text(),'Sam')]"));
     public static final Target REALIZAR_PEDIDO = Target.the("Seleccionar Boton Place Order").located(By.xpath("//div[@class=\"col-lg-1\"]/button"));
 }
